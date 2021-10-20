e16ecc9bb130025ed2fa907f575c7ae2ef0019d2
Austin Loos <54514135+HackChart@users.noreply.github.com>
2021-10-19 17:24:45 -0700

    Repr, Windows Supports Added
    Repr:
    Calling print on the returned wrapper object will display the full results of the test, decoded to UTF-8 for readability.
    Windows Support:
    Wrapper now checks OS at __init__, runs appropriate sub process for OS specific CLI executable.

--- speedtest_wrapper.py
+++ speedtest_wrapper.py
@@ -1,17 +1,24 @@
 import subprocess
+import platform
 
 
+# TODO: definitely rename this class to something less redundant
 class SpeedtestWrapper:
     """Wrapper for Ookla's Speedtest CLI, currently only returns
     results as an object for ease of use"""
     def __init__(self):
-        """Run test and instantiate result attrs"""
-        results = subprocess.run('./speedtest', capture_output=True)
+        # check OS, run appropriate version of speedtest CLI
+        self.os = platform.system()
+        if self.os == 'Windows':
+            results = subprocess.run('.\speedtest.exe', capture_output=True)
+        elif self.os == 'Darwin':
+            results = subprocess.run('./speedtest', capture_output=True)
         # convert stdout from byte string
-        decoded_results = results.stdout.decode('UTF-8')
+        self.decoded_results = results.stdout.decode('UTF-8')
+        print(self.decoded_results)
 
         # init attrs
-        for line in decoded_results.split('\n'):
+        for line in self.decoded_results.split('\n'):
             if 'Server' in line:
                 # TODO: consider expanding to include more granular options for id and loc
                 self.server = line.split(':')[1].strip()
@@ -32,9 +39,20 @@ class SpeedtestWrapper:
             elif 'Upload' in line:
                 # TODO: change sizing
                 # speed as float in Mbps
-                self.download_speed = float(line.split(':')[1].split('Mbps')[0])
+                self.upload_speed = float(line.split(':')[1].split('Mbps')[0])
                 # size as float in MB
-                self.download_size = float(line.split(':')[2].split('MB')[0])
+                self.upload_size = float(line.split(':')[2].split('MB')[0])
             elif 'Packet Loss' in line:
                 # TODO: implement handling for packet loss
                 pass
+
+    def __repr__(self):
+        return self.decoded_results
+
+
+if __name__ == '__main__':
+    # TODO: remove later, for testing purposes only
+    results = SpeedtestWrapper()
+    print(results.upload_speed)
+    print(results.download_speed)
+    print(results)
---
+++ windows_test.py
@@ -0,0 +1,4 @@
+import subprocess
+
+
+subprocess.run('.\speedtest.exe')
