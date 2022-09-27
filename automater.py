8e11c34f5a2134907468e4b90d2c23a5a91c97a1
Austin Loos <54514135+HackChart@users.noreply.github.com>
2022-09-26 18:28:09 -0700

    Added Config Options
    - Added config options for cli_path v log_path, retries, and wait along with some basic error handling.
    - Fixed an issue where packet loss wouldn't be reported, causing csv entries to periodically mismatch in length.
    - Other basic refactoring and reformatting

--- automater.py
+++ automater.py
@@ -3,8 +3,14 @@ import json
 import logging
 import os
 import csv
+from time import sleep
+from sys import exit
 
 
+# TODO: [] CONVERT BYTE DATA TO HUMAN READABLE FORM FOR EXPORT
+# TODO: [] IMPLEMENT LOGGING WHERE APPLICABLE
+# TODO: [] WRITE A README
+# TODO: [] CLEANUP 
 # ----- PROOF OF CONCEPT ----- #
 # designed to monitor network performance by automating tests
 class Speedtest:
@@ -20,18 +26,34 @@ class Speedtest:
                     pass
                 else:
                     # SET CONFIG ATTRS
-                    self.path = config_data['path']
-                    self.output_file = config_data['output']
+                    for key, value in config_data.items():
+                        if key in ['wait', 'retries']:
+                            try:
+                                int(value)
+                            except TypeError as e:
+                                # TODO: LOG E
+                                exit(2)
+                        else:   # assumes all other config entries should be paths
+                            try:
+                                str(value)
+                            except TypeError as e:
+                                # TODO: LOG E
+                                exit(2)
+                        setattr(self, key, value)
         else:
             # TODO: RAISE CUSTOM ERR, LOG, EXIT
-
-            # TODO: LOOK INTO MERGING THIS WITH TRY BLOCK ABOVE
             pass
         try:
-            results = subprocess.check_output([self.path, '-f', 'json'])
+            results = subprocess.check_output([getattr(self, 'cli_path'), '-f', 'json'])
         except FileNotFoundError as err:
-            # TODO: CHANGE TO LOGGING LATER
+            # TODO: CHANGE TO LOGGING LATER, EXIT
             print(f'FileNotFound: {err}]')
+        except subprocess.CalledProcessError as e:
+            # TODO: LOG FAILURE, WAIT, TRY AGAIN
+            for _ in getattr(self, "retries"):
+                sleep(getattr(self, "wait"))
+                # TODO: CHECK FOR RETURN CODE FOR BREAK
+                Speedtest()   # run another test
         else:
             # dynamically set result obj attributes
             jdata = json.loads(results)   # convert results to json
@@ -40,39 +62,40 @@ class Speedtest:
 
     def set_attributes(self, data):
         """Sets obj attributes from json"""
-        for key in data:
-            if type(data[key]) != dict:
-                setattr(self, key, data[key])
+        for key, value in data.items():
+            if type(value) is not dict:
+                setattr(self, key, value)
             else:
-                for nested_key, value in data[key].items():
-                    setattr(self, f'{key}_{nested_key}', value)
+                for nested_key, nested_value in data[key].items():
+                    setattr(self, f'{key}_{nested_key}', nested_value)
 
     def to_csv(self):
-        # REMOVE NON RELEVANT ATTRS FROM ATTRS TO APPEND
-        csv_data = {key: value for key, value in vars(self).items() if key != 'path' and key != 'output_file'}
+        # checks to see if packetLoss needs to be set
+        if getattr(self, "packetLoss", None) is None:
+            setattr(self, "packetLoss", "N/A")
+        # TODO: IMPLEMENT BYTE CONVERSION TO HUMAN READABLE FORMAT
+        # REMOVE CONFIG ATTRS FROM ATTRS TO APPEND
+        config_attrs = ['cli_path', 'log_path', 'output_file', 'retries', 'wait']
+        csv_data = {key: value for key, value in vars(self).items()
+                    if key not in config_attrs}
         # write to file
-        # TODO: FIX - ROWS ARE MISMATCHED FROM TIME TO TIME, POTENTIALLY NOT APPENDING FOR NULL VALUE?
-        # TODO: UPDATE: SEEMS LIKE ITS USUALLY PACKET LOSS THAT DOESNT APPEND
-        # TODO: TEST TO MAKE SURE THAT ONLY PACKET LOSS DROPS
-        # TODO: IMPLEMENT ERROR CHECKING SO THAT N/A VALUES ARE APPENDED FOR MISSING COLS
-        if not os.path.exists(self.output_file):
+        if not os.path.exists(getattr(self, "output_file")):
             # if no file, create header
-            with open(self.output_file, 'w') as f:
+            with open(getattr(self, "output_file"), 'w') as f:
                 writer = csv.DictWriter(f, fieldnames=list(csv_data.keys()))
                 writer.writeheader()
+                # TODO: LOG NEW FILE CREATED AT PATH
         # append result data
-        with open(self.output_file, 'a') as f:
+        with open(getattr(self, "output_file"), 'a') as f:
             writer = csv.DictWriter(f, fieldnames=list(csv_data.keys()))
             writer.writerow(csv_data)
-
-        # TODO: PROBABLY A DIFFERENT OBJ, BUT IMPLEMENT GRAPHING AND BYTE CONVERSION
-        # TODO: FOR READABILITY
-
-        # TODO: TRY TO BREAK ALL OF THIS
+            # TODO: LOG FILE APPENDED PROPERLY
 
 
+
+# TODO: REMOVE LATER, JUST FOR TESTING
 if __name__ == '__main__':
-    for i in range(71):
+    for i in range(2):
         x = Speedtest()
         print(len(vars(x)), vars(x))
         x.to_csv()
